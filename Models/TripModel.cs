c4428c1be2e4e4c13c6bacb083011289fd405489
Eimantas Andrejevas <eimutis.a@gmail.com>
2021-05-26 17:55:27 +0300

    Add project files.

---
+++ Models/TripModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tietoevry_task.Models
+{
+    public class TripModel
+    {
+        public int Distance { get; set; }
+        public string Season { get; set; }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    // Read value on page load
+    $("#result b").html($("#customRange").val());
+
+    // Read value on change
+    $("#customRange").change(function () {
+        $("#result b").html($(this).val());
+    });
+});        
